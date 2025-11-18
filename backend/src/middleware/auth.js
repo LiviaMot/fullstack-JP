eb3bcb40ec9bd855d2c9690bedd36a0c056942bb
Livia <liviamtavares05@gmail.com>
2025-11-17 21:59:00 -0300

    feat: aula Front e Back

---
+++ backend/src/middleware/auth.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken'
+import ServiceUser from '../service/users.js'
+const JWT_SEGREDO = "M3uS3gr3d0"
+
+export default async function authMiddleware(req, res, next) {
+  try {
+    const token = req.headers['authorization']
+    if(!token) {
+      throw new Error("Você não tem permissão para realizar esta ação.")
+    }
+    const decoded = jwt.verify(token.split(' ')[1], JWT_SEGREDO)
+
+    const user = await ServiceUser.FindOne(decoded.id)
+
+    req.headers.user = user
+    next()
+  } catch (erro) {
+    res.status(403).send({
+      data: null,
+      msg: erro.message,
+      error: true
+    })
+  }
+}
---
+++ backend/src/service/users.js
@@ -0,0 +1,82 @@
+import User from '../model/users.js'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+
+const JWT_SEGREDO = "M3uS3gr3d0"
+const SALT = 10 // 12
+
+class ServiceUser {
+
+  FindAll() {
+    return User.findAll()
+  }
+
+  async FindOne(id) {
+    if (!id) {
+      throw new Error("Favor informar o ID")
+    }
+
+    // preciso procurar um usuario no banco
+    const user = await User.findByPk(id)
+
+    if (!user) {
+      throw new Error(`Usuário ${id} não encontrado`)
+    }
+
+    return user
+  }
+
+  async Create(nome, email, senha, ativo, permissao) {
+    if (!nome || !email || !senha) {
+      throw new Error("favor preencher todos os campos")
+    }
+
+    const senhaCriptografada = await bcrypt.hash(String(senha), SALT)
+
+    await User.create({
+      nome, email, senha: senhaCriptografada, ativo, permissao
+    })
+  }
+
+  async Update(id, nome) {
+    const oldUser = User.findByPk(id)
+    oldUser.senha = senha
+      ? await bcrypt.hash(String(senha), SALT) // if
+      : oldUser.senha // else
+
+    // User.Update(id, nome)
+  }
+
+  async Delete(id) {
+    const oldUser = User.findByPk(id)
+
+    if (!oldUser) {
+      throw new Error(`Usuário ${id} não encontrado`)
+    }
+    
+    await User.destroy(id)
+  }
+
+  async Login(email, senha) {
+    if(!email || !senha) {
+      throw new Error("Email ou senha inválidos.")
+    }
+
+    const user = await User.findOne({ where: { email } })
+
+    if (
+      !user 
+      || !(await bcrypt.compare(String(senha), user.senha))
+    ) {
+      throw new Error("Email ou senha inválidos.")
+    }
+
+    return jwt.sign(
+      { id: user.id, nome: user.nome, permissao: user.permissao },
+      JWT_SEGREDO,
+      { expiresIn: 60 * 60 }
+    )
+  }
+}
+
+export default new ServiceUser()
